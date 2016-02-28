9ffd987e10dcb6a1bf7e5b3aacc431d354305b25
Yurii <smile.oym@gmail.com>
2016-02-28 14:39:02 +0200

    Added files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function sayHello() {
+    document.getElementById("demo").innerHTML =
+		'<h1 style="color:green">Hello!</h1>';
+}
+
+function sayGoodbuy() {
+    document.getElementById("demo").innerHTML =
+		'<h1 style="color:red">Goodbuy!</h1>';
+}
